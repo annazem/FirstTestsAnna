8da43e7db3aee44252d22562af0147cc2ea4b428
anna.stryukova <anna.stryukova@sbermarket.ru>
2023-09-06 12:24:43 +0300

    Add AfterMethod

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -1,23 +1,26 @@
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class FirstTests {
     private int a;
     private int b;
+
+    @AfterMethod(alwaysRun = true)
+    void afterMethod(){
+        System.out.println();
+    }
 
     @BeforeClass(alwaysRun = true)
     void beforeClass() {
         a = 20;
         b = 4;
         System.out.println("Проверим, работает ли оно?");
+        System.out.println();
     }
 
     @AfterClass(alwaysRun = true)
     void afterClass() {
         System.out.println("Работает!!!");
-        System.out.println();
     }
 
     @Test(priority = 1, description = "Первый тест - Сложение")
@@ -25,7 +28,6 @@ public class FirstTests {
         System.out.println("Посчитаем сумму чисел 20 и 4");
         int sum = a + b;
         System.out.println("Сумма чисел: " + sum);
-        System.out.println();
         Assert.assertEquals(24, sum, "Что-то пошло не так! Сумма считается не верно!");
 
     }
@@ -35,7 +37,6 @@ public class FirstTests {
         System.out.println("Посчитаем разницу чисел 20 и 4");
         int sub = a - b;
         System.out.println("Разница чисел: " + sub);
-        System.out.println();
        Assert.assertEquals(16, sub, "Что-то пошло не так! Разница считается не верно!");
     }
 
@@ -44,7 +45,6 @@ public class FirstTests {
         System.out.println("Умножим числа 20 и 4");
         int mul = a * b;
         System.out.println("Произведение чисел: " + mul);
-        System.out.println();
         Assert.assertEquals(80, mul, "Что-то пошло не так! Произведение считается не верно!");
     }
 
@@ -53,7 +53,6 @@ public class FirstTests {
         System.out.println("Разделим числа 20 и 4");
         int div = a / b;
         System.out.println("Частное от деления чисел: " + div);
-        System.out.println();
         Assert.assertEquals(5, div, "Что-то пошло не так! Частное считается не верно!");
     }
 }
